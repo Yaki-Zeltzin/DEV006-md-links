c360a6ebdae9274a9d16aa9fd1584c9e7ba7a217
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-07-06 10:26:39 -0600

    Function mdLinks is added

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-module.exports = () => {
-  // ...
-};
+const validate = false;
+
+function mdLinks(path, options){
+  return new Promise((resolve, reject) => {
+    if(validate) {
+      resolve('El link es válido');
+    }else{
+      reject('El mdLinks esta roto');
+    }
+  })
+}
 
 
-//const linkRegex = /\[(.*)\]\((https?:\/\/[\w\d./?=#]+)\)/g;
+mdLinks().then(res => console.log(res))
+.catch(err => console.log(err))
+//const linkRegex expresion irregular= /\[(.*)\]\((https?:\/\/[\w\d./?=#]+)\)/g;
