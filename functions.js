f95a18d7d3c7bb2423303685884e546957923afd
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-07-10 22:51:33 -0600

    The readMDFile function that reads MD file is created

--- functions.js
+++ functions.js
@@ -1,25 +1,79 @@
 const path = require('path')
-const { access, constants } = require('node:fs')
+const { access, stat, readdir } = require('node:fs')
+const { readFile } = require('fs')
 
-function isAbsolute (route) {
+
+function isAbsolute(route) {
   return path.isAbsolute(route) //indica si la ruta es absoluta
 };
 
-function relativeToAbsolute (route) {//convierte relativa a absoluta
+function relativeToAbsolute(route) {//convierte relativa a absoluta
   return path.resolve(route)
 }
 
- function isValidePath(route){
+function isValidePath(route) {
   return new Promise((resolve) => {
     access(route, constants.F_OK, (err) => {//valida que exista el documento
-      if(err){ //ruta no existe
+      if (err) { //ruta no existe
         resolve(false);//la ruta no existe
       } else {
         resolve(true);//la ruta existe
       }
     })
   })
- };
+};
+
+function isFile(route) {
+  return new Promise((resolve, reject) => {
+    stat(route, (err, stats) => {
+      if (err) {
+        reject(err)
+      } else if (stats.isFile()) {
+        resolve(true)
+      } else if (stats.isDirectory()) {
+        resolve(false)
+      }
+    });
+  });
+};
+
+function getFilesInDirectory(directoryPath) {
+  return new Promise((resolve, reject) => {
+    readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.log(err); //Reachaza la promesa si hay un error al leer el directorio
+      } else {
+        const filePaths = files
+          .map((file) => path.join(directoryPath, file))//Devuelve ruta de directorio + archivo
+          .filter((filePath) => path.extname(filePath) === '.md') //extrae la ruta de archivo y busca el que tiene extensión md
+
+          //files.forEach(a=>{
+          //return a.includes('.md')
+          //});
+
+        if (filePaths.length === 0) {
+          reject('El directorio no contiene archivos MD');
+        } else {
+          resolve(filePaths);
+        };
+      };
+    });
+  });
+};
+
+function readMDFile(route) {
+  return new Promise((resolve, reject) => {
+    readFile(route, 'utf8', (err, data) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(data);
+      };
+    });
+  });
+};
+
+readMDFile('C:./README.md')
 
 
 
@@ -27,4 +81,6 @@ module.exports = {
   isAbsolute,
   relativeToAbsolute,
   isValidePath,
+  getFilesInDirectory,
+  readMDFile,
 }
